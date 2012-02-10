dd0051ad067d08a3e22df8b0fd7fc6f369c71091
Yoshikazu Miyoshi <yoshikazum@gmail.com>
2012-02-11 01:54:59 +0900

    initial commit

---
+++ SampleLicensesTableViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleLicensesTableViewController
+//
+//  Created by 三好 良和 on 12/02/11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
